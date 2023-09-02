b74fd5d662efdcea05f103fc6bfda56ee4fe221a
Guilherme Ferreira <guiferreiramoraes@outlook.com>
2023-09-02 02:23:34 -0300

    fix: vinyl images

--- src/components/Vinyl/Vinyl.jsx
+++ src/components/Vinyl/Vinyl.jsx
@@ -5,8 +5,8 @@ const Vinyl = () => {
         <>
             <div className="vinyl-box">
                 <div className='vinyl rotate' >
-                    <img src="src\assets\vinyl-example.jpg" alt="" className='vinyl-image' />
-                    <img src="src\assets\vinyl-center.png" alt="" className='vinyl-center' />
+                    <img src="\vinyl-example.jpg" alt="" className='vinyl-image' />
+                    <img src="\vinyl-center.png" alt="" className='vinyl-center' />
                 </div>
             </div>
         </>
